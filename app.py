8d223b86e23571f8fc6a6d58c7357c29ad752517
Frederik Carrapato <230323068@estudantes.ips.pt>
2025-07-01 17:02:16 +0100

    adição de categorias

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import Flask, render_template
+from models import db, Categoria
 
 app = Flask(__name__)
+
+@app.before_first_request
+def criar_tabelas():
+    db.connect()
+    db.create_tables([Categoria], safe=True)
+    
+      # Categorias padrão
+    categorias = ["Económico", "Silver", "Gold"]
+    for nome in categorias:
+        if not Categoria.select().where(Categoria.nome == nome).exists():
+            Categoria.create(nome=nome)
 
 @app.route("/")
 def index():
